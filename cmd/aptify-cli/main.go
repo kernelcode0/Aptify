5dced89b9fe72d6865403d7b0082b9b8fcd0cb3b
Wajahat Ali <wajahat.ali@rolustech.com>
2026-09-07 12:05:48 +0500

    feat: update version to v1.1.1 in main.go

--- cmd/aptify-cli/main.go
+++ cmd/aptify-cli/main.go
@@ -18,7 +18,6 @@ import (
 )
 
 // version is overridden by release builds via -ldflags "-X main.version=vX.Y.Z".
-var version = "v1.1.0"
 var version = "v1.1.1"
 
 // Config is stored at ~/.config/aptify/config.json.
